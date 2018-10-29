cf22f1e435c5237987136a68388d330ee3c78260
RenatoQuiroz <renatoquirozm@gmail.com>
2018-10-29 17:03:20 -0300

    nombre

--- MisPerris/MisPerris/wsgi.py
+++ MisPerris/MisPerris/wsgi.py
@@ -14,17 +14,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MisPerris.settings')
 
 application = get_wsgi_application()
-<<<<<<< HEAD
-=======
-
-asxniasxiabsxbas
-ascascasc
-ascascascas
-ascascascasc
-ksaddsa
->>>>>>> 3f292483d31a29d45afaac3d233726e323f5d09f
-
-asfghjkxcmsopdcm
-sdfcsad
-cdasdcascas
-ascasc
