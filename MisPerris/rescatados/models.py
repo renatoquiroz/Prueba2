722a084c19225c399311a94da4380f03858c08e6
EspectroXd <44536977+EspectroXd@users.noreply.github.com>
2018-10-30 23:33:31 -0300

    Listar en HTML con BD

--- MisPerris/rescatados/models.py
+++ MisPerris/rescatados/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class FormularioAdopcion(models.Model):
+    rut = models.CharField(max_length=12, help_text="Ej: 12.123.123-1" , unique=True)
+    correo = models.CharField(max_length=100)
+    nombre = models.CharField(max_length=60, help_text="Nombre Completo")
+    fechaNac = models.DateTimeField(default=timezone.now)
+    region = models.CharField(max_length=60, help_text="region")
+    ciudad = models.CharField(max_length=60, help_text="ciudad")
+    tipoVivienda = models.CharField(max_length=60, help_text="vivienda")
+
+#cada vez se configura algo de la tabla o BD
+#python manage.py makemigrations
+#pythons manage.py migrate
+
+#guardar en git
+#git commit -a -m "nombre commit"
+#git push -u origin master
+
+    def __str__(self):
+        return self.rut
--- MisPerris/rescatados/views.py
+++ MisPerris/rescatados/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import FormularioAdopcion
 
 # Create your views here.
 def formulario(request):
-    return render(request,'formularios/formulario.html')
+    FormAdopAll = FormularioAdopcion.objects.all().order_by('nombre')
+    return render(request,'formularios/formulario.html', {'FormAdopAll':FormAdopAll})
